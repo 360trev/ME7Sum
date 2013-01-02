21856973d102a48af27cb4bb0ad2016ae2c58f0b
360trev <trevormensah@hotmail.com>
2013-01-02 14:38:47 +0100

    Update inifile/inifile.c
    for compiling under win32

--- inifile/inifile.c
+++ inifile/inifile.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-#include <unistd.h>
+#include <unistd.h> // added back in, unable to compile under win32 mingw without it. (for write() function)
 #include "inifile.h"
 
 static char *trimstr(char *s, char *end)
