93fbecb12c739b808c6796b83634dfebbd95089d
Nye Liu <nyet@nyet.org>
2013-01-02 10:15:17 -0800

    Default to mingw under cygwin

--- vars.mk
+++ vars.mk
@@ -6,8 +6,9 @@ CFLAGS  =-Wall -O3 -Werror
 
 UNAME := $(shell uname -s)
 ifeq ($(findstring CYGWIN,$(UNAME)),CYGWIN)
-# CFLAGS += -D_WIN32_
 EXE_EXT := .exe
+CC=i686-pc-mingw32-gcc
+CFLAGS += -D_GNU_SOURCE=1
 else
 endif
 
