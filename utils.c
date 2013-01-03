d44039daea77b217901366a9d16bc742447f45a4
Nye Liu <nyet@nyet.org>
2013-01-02 18:01:33 -0800

    Just put it all in utils.c We already have too many files

--- utils.c
+++ utils.c
@@ -1,3 +1,27 @@
+/* Permission is hereby granted, free of charge, to any person obtaining
+   a copy of this software and associated documentation files (the
+   "Software"), to deal in the Software without restriction, including
+   without limitation the rights to use, copy, modify, merge, publish,
+   distribute, sublicense, and/or sell copies of the Software, and to
+   permit persons to whom the Software is furnished to do so, subject to
+   the following conditions:
+
+   The above copyright notice and this permission notice shall be
+   included in all copies or substantial portions of the Software.
+
+   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+   EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+   OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+   NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+   BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN
+   AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF
+   OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+   IN THE SOFTWARE.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
 #include "utils.h"
 
 int iload_file(struct ImageHandle *ih, const char *fname, int rw)
@@ -14,4 +38,68 @@ int ifree_file(struct ImageHandle *ih)
 	if((ih->d.p) != 0) free(ih->d.p);
 	memset(ih, 0, sizeof(*ih));
 	return 0;
+}
+
+/* load a file into memory and return buffer */
+uint8_t *load_file(const char *filename, size_t *filelen)
+{
+	FILE *fp;
+	uint8_t *data;
+	size_t size,bytesRead;
+
+	/* open file */
+	printf("þ Opening '%s' file\n",filename);
+	if ((fp = (FILE *)fopen(filename, "rb")) == NULL){ printf("\nCan't open file \"%s\".", filename); return(0); }
+
+	/* get file length */
+	printf("þ Getting length of '%s' file\n",filename);
+	fseek(fp, 0, SEEK_END);
+	size = ftell(fp);
+	fseek(fp, 0, SEEK_SET);
+	if(size <= 0) { printf("Error: Problem with seeking filesize\n"); fclose(fp); return(0); }
+
+	*filelen = size;		/* return size of file to caller */
+
+	/* alloc buffer for file */
+	printf("þ Allocating buffer of %zd bytes\n",size);
+	data = (uint8_t *)malloc(size);
+	if(data == 0) { printf("\nfailed to allocate memory to load module\n"); fclose(fp); return 0; }
+
+	/* load file into buffer */
+	printf("þ Reading file to buffer\n");
+	bytesRead = fread(data, 1, size, fp);
+
+	/* validate it all loaded correctly */
+	printf("þ Validating size correct %zd=%zd\n",bytesRead,size);
+	if(bytesRead != size) { printf("\nfailed to load module into buffer\n"); free(data); fclose(fp); return 0; }
+
+	/* close the file */
+	printf("þ Closing file\n\n");
+	fclose(fp);
+	return(data);
+}
+
+/* load a file into memory and return buffer */
+int save_file(const char *filename, const uint8_t *filebuf, size_t filelen)
+{
+	FILE *fp;
+	size_t bytesWritten;
+
+	/* open file */
+	printf("þ Opening '%s' file for writing\n",filename);
+	if ((fp = (FILE *)fopen(filename, "wb")) == NULL){ printf("\nCan't open file \"%s\".", filename); return(-1); }
+
+	/* load file into buffer */
+	printf("þ Writing to file\n");
+	bytesWritten = fwrite((void *)filebuf, (size_t)1, (size_t)filelen, fp);
+
+	/* validate it all loaded correctly */
+	printf("þ Validating size correct %d=%d\n",(int)bytesWritten,(int)filelen);
+	if(bytesWritten != filelen) { printf("\nfailed to write buffer\n"); fclose(fp); return(-2); }
+
+	/* close the file */
+	printf("þ All OK, closing file\n\n");
+	fclose(fp);
+
+	return(0);
 }
--- utils.h
+++ utils.h
@@ -1,3 +1,25 @@
+/*
+   Permission is hereby granted, free of charge, to any person obtaining
+   a copy of this software and associated documentation files (the
+   "Software"), to deal in the Software without restriction, including
+   without limitation the rights to use, copy, modify, merge, publish,
+   distribute, sublicense, and/or sell copies of the Software, and to
+   permit persons to whom the Software is furnished to do so, subject to
+   the following conditions:
+
+   The above copyright notice and this permission notice shall be
+   included in all copies or substantial portions of the Software.
+
+   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+   EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+   OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+   NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+   BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN
+   AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF
+   OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+   IN THE SOFTWARE.
+*/
+
 #ifndef _UTILS_H
 #define _UTILS_H
 
@@ -7,9 +29,6 @@
 #else
 #include <endian.h>
 #endif
-
-#include "load_file.h"
-#include "save_file.h"
 
 struct ImageHandle {
 	union {
@@ -50,5 +69,7 @@ struct ImageHandle {
 
 int iload_file(struct ImageHandle *ih, const char *fname, int rw);
 int ifree_file(struct ImageHandle *ih);
+int save_file(const char *filename, const uint8_t *filebuf, size_t filelen);
+uint8_t *load_file(const char *filename, size_t *filelen);
 
 #endif
