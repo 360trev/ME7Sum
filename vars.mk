e601787b309fd98f664dc5a1467decb40dab56cd
Nye Liu <nyet@nyet.org>
2013-01-05 21:40:54 -0800

    Prepare for getting version from git

--- vars.mk
+++ vars.mk
@@ -1,15 +1,18 @@
-RM      =rm -f
-CC      =gcc
-AR      =ar rcs
-ECHO    =@echo
-CFLAGS  =-Wall -O3 -Werror -MD
+RM	= rm -f
+CC	= gcc
+AR	= ar rcs
+ECHO	= @echo
+CFLAGS	= -Wall -O3 -Werror -MD $(CDEFS)
 
-UNAME := $(shell uname -s)
+CDEFS	+= -D__GIT_VERSION=\"$(GIT_VERSION)\"
+
+UNAME       = $(shell uname -s)
+GIT_VERSION = $(shell sh -c 'git describe --abbrev=4 --dirty --always')
+
 ifeq ($(findstring CYGWIN,$(UNAME)),CYGWIN)
-EXE_EXT := .exe
-CC=i686-pc-mingw32-gcc
-CFLAGS += -D_GNU_SOURCE=1
-else
+EXE_EXT = .exe
+CC	= i686-pc-mingw32-gcc
+CDEFS	+= -D_GNU_SOURCE=1
 endif
 
-SRC     =$(notdir $(foreach dir, ., $(wildcard $(dir)/*.c)))
+SRC     = $(notdir $(foreach dir, ., $(wildcard $(dir)/*.c)))
